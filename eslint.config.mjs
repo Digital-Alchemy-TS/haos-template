74c1df91e02b1eeccceb17780156d1a2b918243f
Zoe <git@zoe-codez.xyz>
2024-09-25 09:13:16 -0500

    updates (#6)
    * tweaks
    
    * bumps

---
+++ eslint.config.mjs
@@ -0,0 +1,61 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import _import from "eslint-plugin-import";
+import prettier from "eslint-plugin-prettier";
+import unicorn from "eslint-plugin-unicorn";
+import { fixupPluginRules } from "@eslint/compat";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  ...compat.extends(
+    "plugin:@typescript-eslint/recommended",
+    "plugin:unicorn/recommended",
+    "plugin:prettier/recommended",
+  ),
+  {
+    files: ["src/**/*.ts"],
+    plugins: {
+      "@typescript-eslint": typescriptEslint,
+      import: fixupPluginRules(_import),
+      prettier,
+      unicorn,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+
+      parser: tsParser,
+      ecmaVersion: 5,
+      sourceType: "module",
+      parserOptions: {
+        project: "tsconfig.json",
+      },
+    },
+    rules: {
+      "@typescript-eslint/interface-name-prefix": "off",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-namespace": "off",
+      "unicorn/prefer-top-level-await": "off",
+      "unicorn/import-style": "off",
+      "unicorn/prefer-module": "off",
+      "unicorn/no-null": "off",
+      "unicorn/prevent-abbreviations": "off",
+    },
+  },
+];
